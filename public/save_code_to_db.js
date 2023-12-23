ceefa1c5dfd25876cca92c39b44691c200810503
Abdul Ghaffar soomro <124041090+AgsBrand@users.noreply.github.com>
2023-12-23 20:10:20 +0500

    done full

--- public/save_code_to_db.js
+++ public/save_code_to_db.js
@@ -1,17 +1,19 @@
-let playBtn = document.getElementById("playBtn")
-playBtn.addEventListener("click", async () => {
-    console.log("Clicked btn")
-    let editor = document.getElementById("editor").value
-    let email = localStorage.getItem("email")
-    let body = { code: editor, email }
-    console.log(body)
-    let resp = await fetch('/api/save-code', {
-        method: "post",
-        body: JSON.stringify(body),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-    let result = await resp.json()
-    console.log(result)
-})
+// let playBtn = document.getElementById("playBtn")
+// let codeDiv = document.getElementById("editor")
+
+// playBtn.addEventListener("click", async () => {
+//     // let code = codeDiv.getValue()
+//     console.log(code)
+//     let email = localStorage.getItem("email")
+//     let body = { code, email }
+//     console.log(body)
+//     let resp = await fetch('/api/save-code', {
+//         method: "post",
+//         body: JSON.stringify(body),
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     })
+//     let result = await resp.json()
+//     console.log(result)
+// })
